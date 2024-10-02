aeed04c57db52cd20856a4f062da1405ebd317c0
aquilsm <aquilmohammed4@gmail.com>
2024-10-02 20:21:20 +0530

    menu builder category crud

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'vendor',
+    'menu',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -149,3 +151,5 @@ EMAIL_HOST_USER=config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS=True
 DEFAULT_FROM_EMAIL= 'foodOnline Marketplace <aquilmohammed4@gmail.com>'
+
+GOOGLE_API_KEY = 'AIzaSyDDJNnjQv7YPze1tfBihY1f7ayRGDEsbZk'
